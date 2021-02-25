d7b3192097779f954a89d47b01f8a820a864575f
Vtchoo <vittor-limma@hotmail.com>
2021-02-25 13:13:51 -0300

    Add {level} to user profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile() {
     
+    const { level } = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
             <img src='https://github.com/Vtchoo.png' alt='Foto de perfil' />
@@ -9,7 +13,7 @@ export default function Profile() {
                 <strong>Vtchoo</strong>
                 <p>
                     <img src="/icons/level.svg" alt="level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
