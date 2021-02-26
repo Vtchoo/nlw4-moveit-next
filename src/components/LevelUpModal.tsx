1f6c81ca488d71ed9ce37c8de208623510b159b9
Vtchoo <vittor-limma@hotmail.com>
2021-02-26 12:37:24 -0300

    Create LevelUpModal

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,24 @@
+import { prependOnceListener } from 'process'
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/LevelUpModal.module.css'
+
+export function LevelUpModal() {
+
+    const { level, closeLevelUpModal } = useContext(ChallengesContext)
+
+    return (
+        <div className={styles.overlay}>
+            <div className={styles.container}>
+                <header>{level}</header>
+                
+                <strong>Congratulations!</strong>
+                <p>You're reached a new level!</p>
+
+                <button type='button' onClick={closeLevelUpModal}>
+                    <img src="/icons/close.svg" alt="Close dialog" />
+                </button>
+            </div>
+        </div>
+    )
+}
